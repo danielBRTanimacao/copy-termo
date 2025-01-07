10d52217656632e21e132afa8337693007dfa23b
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2025-01-07 11:34:19 -0300

    feat: style in header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import Game from "./components/Game";
 import Header from "./components/Header";
+import Game from "./components/Game";
 import Keyboard from "./components/Keyboard";
 
 export default () => {
     return (
         <>
             <Header />
-            <Game />
-            <Keyboard />
+            {/* <Game />
+            <Keyboard /> */}
         </>
     );
 };
