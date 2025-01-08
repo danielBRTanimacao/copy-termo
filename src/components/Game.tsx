9d937bb4d7272c75d6602e0421485105d794fc35
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2025-01-08 12:07:03 -0300

    feat: created first logical on game and desgin

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -9,11 +9,11 @@ export default () => {
                 <input type="text" maxLength={1} required />
             </div>
             <div>
-                <input className="empty" type="text" maxLength={1} required />
-                <input className="empty" type="text" maxLength={1} required />
-                <input className="empty" type="text" maxLength={1} required />
-                <input className="empty" type="text" maxLength={1} required />
-                <input className="empty" type="text" maxLength={1} required />
+                <input className="empty" type="text" maxLength={1} disabled />
+                <input className="empty" type="text" maxLength={1} disabled />
+                <input className="empty" type="text" maxLength={1} disabled />
+                <input className="empty" type="text" maxLength={1} disabled />
+                <input className="empty" type="text" maxLength={1} disabled />
             </div>
         </main>
     );
