933227f511a37dde19bbdb69bb009b4aeff0b80e
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2025-01-17 10:52:56 -0300

    feat: first version project

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,27 +1,43 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Words from "../assets/json/words.json";
 
 export default () => {
-    const [getRandomTermo] = useState(
+    const [randomTermo] = useState(
         Words[Math.floor(Math.random() * Words.length)]
     );
-    const [valuesArray, setInputValues] = useState<string[]>(Array(5).fill(""));
+    const [counter, setCounter] = useState(1);
+    const [arrayBoolean, setArrayBoolean] = useState(
+        Array(6)
+            .fill(true)
+            .map((_, index) => index !== 0)
+    );
     const [isCorrect, setIsCorrect] = useState(false);
-    const termoDigited = valuesArray.join("").toUpperCase();
-
-    const handleChange = (
-        index: number,
-        event: React.ChangeEvent<HTMLInputElement>
-    ) => {
-        const newValues = [...valuesArray];
-        newValues[index] = event.target.value;
-        setInputValues(newValues);
-    };
+    const inputRefs = useRef<HTMLInputElement[]>([]);
 
     const handleCollectValues = () => {
-        console.log("enviado " + termoDigited);
-        if (termoDigited === getRandomTermo) {
-            console.log("acertou termo " + getRandomTermo);
+        const values = inputRefs.current.map((input) => input?.value || "");
+        const termo = values.join("").toUpperCase();
+        if (termo.length < 5) {
+            return null;
+        }
+
+        if (randomTermo === termo) {
+            setIsCorrect(true);
+            setArrayBoolean(
+                Array(6)
+                    .fill(true)
+                    .map((_, index) => index !== -1)
+            );
+        } else {
+            setCounter((prevCounter) => {
+                const newCounter = prevCounter + 1;
+                const newIsEmpty = Array(6)
+                    .fill(true)
+                    .map((value, index) => index !== newCounter - 1);
+                setArrayBoolean(newIsEmpty);
+
+                return newCounter;
+            });
         }
     };
 
@@ -35,24 +51,32 @@ export default () => {
         <main>
             {isCorrect ? (
                 <div className="win">
-                    <p>Acertou!</p>
+                    <p>Incrivel!</p>
                 </div>
             ) : (
-                <div className="win">{getRandomTermo}</div>
+                <div className="win">{randomTermo}</div>
             )}
-            <div onKeyDown={handleKeyDown}>
-                {valuesArray.map((value, index) => (
-                    <input
-                        key={index}
-                        className=""
-                        type="text"
-                        maxLength={1}
-                        value={value}
-                        onChange={(e) => handleChange(index, e)}
-                        disabled={false}
-                    />
-                ))}
-            </div>
+            {Array.from({ length: 6 }).map((_, rowKeys) => (
+                <div key={rowKeys} onKeyDown={handleKeyDown}>
+                    {Array.from({ length: 5 }).map((_, colKeys) => (
+                        <input
+                            key={`disabled-${colKeys}`}
+                            className={arrayBoolean[rowKeys] ? `empty` : ""}
+                            type="text"
+                            maxLength={1}
+                            required
+                            disabled={arrayBoolean[rowKeys]}
+                            ref={
+                                arrayBoolean[rowKeys]
+                                    ? null
+                                    : (el) =>
+                                          el &&
+                                          (inputRefs.current[colKeys] = el)
+                            }
+                        />
+                    ))}
+                </div>
+            ))}
         </main>
     );
 };
