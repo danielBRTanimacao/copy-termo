5c075312d4107551526479bb8b181f99cd2223cd
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2025-01-18 18:22:05 -0300

    creating a about me and a url search

--- src/About.tsx
+++ src/About.tsx
@@ -1,17 +1,31 @@
 export default () => {
     return (
-        <main>
-            <img src="my_image" alt="my-img" />
+        <main className="about">
+            <img
+                src="https://media.licdn.com/dms/image/v2/D4D03AQGCD3hfqVbyyw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1720781719866?e=1742428800&v=beta&t=4wxnw-ne-PnhqwNjN3GEe5Gj3vLXuLan5PChOa4mzYU"
+                alt="my-img"
+            />
             <h1>Iae, tudo bem?</h1>
 
-            <div>
-                <h2>teste</h2>
-                <a href="https://term.ooo/sobre/">Criador original</a>
-            </div>
-            <div>
-                <h2>teste</h2>
-                <a href="https://term.ooo/sobre/">Criador original</a>
-            </div>
+            <p>
+                O <b>Termo</b> foi criado em Janeiro de 2022 por,{" "}
+                <a href="https://term.ooo/sobre/" target="blank">
+                    Fernando Serboncini
+                </a>
+                .
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+                eaque modi, in repudiandae optio saepe?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+                eaque modi, in repudiandae optio saepe?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+                eaque modi, in repudiandae optio saepe?
+            </p>
         </main>
     );
 };
