c438c951dc8f6d27df207d7c9b591a8a6d7b6067
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2025-01-18 12:52:02 -0300

    update about

--- src/About.tsx
+++ src/About.tsx
@@ -1,9 +1,17 @@
 export default () => {
     return (
         <main>
-            <header>
-                <h1>Sobre o projeto</h1>
-            </header>
+            <img src="my_image" alt="my-img" />
+            <h1>Iae, tudo bem?</h1>
+
+            <div>
+                <h2>teste</h2>
+                <a href="https://term.ooo/sobre/">Criador original</a>
+            </div>
+            <div>
+                <h2>teste</h2>
+                <a href="https://term.ooo/sobre/">Criador original</a>
+            </div>
         </main>
     );
 };
