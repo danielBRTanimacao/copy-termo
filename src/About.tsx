fec20b1546570ea276a39f276ab76d87292a2f0b
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2025-01-19 10:04:35 -0300

    feat: conclusion of about

--- src/About.tsx
+++ src/About.tsx
@@ -15,17 +15,41 @@ export default () => {
                 .
             </p>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-                eaque modi, in repudiandae optio saepe?
+                Este projeto é um clone do jogo Termo, desenvolvido com{" "}
+                <b>React</b> e {""}
+                <b>TypeScript</b>.
             </p>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-                eaque modi, in repudiandae optio saepe?
+                Esta versão oferece uma experiência simples e interativa para os
+                usuários tentarem adivinhar a palavra do dia, mantendo a lógica
+                do jogo original com a mesma interface moderna e responsiva.
             </p>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-                eaque modi, in repudiandae optio saepe?
+                Todas as palavras estão armazenadas em um arquivo <b>JSON</b>.
+                Não a uso de <b>Cookies</b> ou coleta de algum dado do usuario
             </p>
+            <p>
+                Aqui esta meu{" "}
+                <a href="https://github.com/danielBRTanimacao" target="blank">
+                    GitHub
+                </a>{" "}
+                e outras redes sociais{" "}
+                <a
+                    href="https://www.linkedin.com/in/daniel-tenório-6471b0244/"
+                    target="blank"
+                >
+                    Linkedin
+                </a>
+                . Por fim{" "}
+                <a
+                    href="https://github.com/danielBRTanimacao/copy-termo"
+                    target="blank"
+                >
+                    link do projeto
+                </a>
+                .
+            </p>
+            <footer>Todos os direitos reservados Fernando Serboncini.</footer>
         </main>
     );
 };
