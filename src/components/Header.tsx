2e510f1b9124d2190c7e65ccc59ebbcd31aab0e9
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2025-01-19 10:54:39 -0300

    feat: created help model for user

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -110,6 +110,40 @@ export default () => {
                     tentativa, as peças mostram o quão perto você está da
                     solução.
                 </p>
+                <div className="exemple">
+                    <span className="letter correct">T</span>
+                    <span className="letter">U</span>
+                    <span className="letter">R</span>
+                    <span className="letter">M</span>
+                    <span className="letter">A</span>
+                </div>
+                <p>
+                    A letra <span className="letter correct">T</span> faz parte
+                    da palavra e está na posição correta.
+                </p>
+                <div className="exemple">
+                    <span className="letter">V</span>
+                    <span className="letter">I</span>
+                    <span className="letter place">O</span>
+                    <span className="letter">L</span>
+                    <span className="letter">A</span>
+                </div>
+                <p>
+                    A letra <span className="letter place">O</span> faz parte da
+                    palavra porem não esta na posição correta.
+                </p>
+                <div className="exemple">
+                    <span className="letter">P</span>
+                    <span className="letter">U</span>
+                    <span className="letter">L</span>
+                    <span className="letter wrong">G</span>
+                    <span className="letter">A</span>
+                </div>
+                <p>
+                    A letra <span className="letter wrong">G</span> não faz
+                    parte da palavra.
+                </p>
+                <p>A uma palavra nova todo dia.</p>
             </Modal>
             <Modal
                 isOpen={isOpen.status}
