f42b0c824c18a0dbf53f45bd5aaca0b7e2b88b16
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2025-01-26 10:05:31 -0300

    creating new modal for multiplayer

--- src/components/MultiPlayer.tsx
+++ src/components/MultiPlayer.tsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
+import Modal from "./Modal/Modal";
 import Header from "./Header";
 
 export default () => {
+    const [isOpen, setOpen] = useState({
+        enterRoom: false,
+        createRoom: false
+    });
     const [playerNameInput, setPlayerNameInput] = useState("");
     const [savedPlayerName, setSavedPlayerName] = useState("");
 
@@ -61,6 +66,7 @@ export default () => {
     const [roomId, setroomId] = useState("");
     const createRoom = () => {
         setroomId(generatePlayerId(true));
+
         console.log(roomId);
         return "Aoba";
     };
@@ -103,30 +109,6 @@ export default () => {
                                 />
                             </div>
                             <div>
-                                {/* <h2
-                                    style={{
-                                        display: "flex",
-                                        alignItems: "center"
-                                    }}
-                                >
-                                    SUA SALA {roomId}{" "}
-                                    <button>
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            width="16"
-                                            height="16"
-                                            fill="currentColor"
-                                            className="bi bi-copy"
-                                            viewBox="0 0 16 16"
-                                        >
-                                            <path
-                                                fill-rule="evenodd"
-                                                d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"
-                                            />
-                                        </svg>
-                                    </button>
-                                </h2> */}
-
                                 <input
                                     className="initBtn"
                                     type="button"
@@ -138,6 +120,39 @@ export default () => {
                     </div>
                 )}
             </main>
+            <Modal
+                isOpen={isOpen.enterRoom}
+                onClose={() =>
+                    setOpen({
+                        enterRoom: false,
+                        createRoom: isOpen.createRoom
+                    })
+                }
+            >
+                <h2
+                    style={{
+                        display: "flex",
+                        alignItems: "center"
+                    }}
+                >
+                    SUA SALA {roomId}{" "}
+                    <button>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            className="bi bi-copy"
+                            viewBox="0 0 16 16"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"
+                            />
+                        </svg>
+                    </button>
+                </h2>
+            </Modal>
         </>
     );
 };
